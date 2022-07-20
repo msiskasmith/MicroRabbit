6bbd77e1c444cc51af9942c8f2ae6c345ebc5285
Smith <msiskasmith@gmail.com>
2022-07-21 00:03:53 +0200

    Initial Commit of Infra Dependency Project

---
+++ MicroRabbit.Infrastructure.InversionOfControl/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infrastructure.InversionOfControl
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
