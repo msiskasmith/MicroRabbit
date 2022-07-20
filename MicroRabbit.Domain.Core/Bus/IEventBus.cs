2f26263e8327e32779799d1230cb6f7c3f76ffe3
Smith <msiskasmith@gmail.com>
2022-07-20 22:29:18 +0200

    Move folders from Domain Directory to Domain Core Project

---
+++ MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    internal interface IEventBus
+    {
+    }
+}
